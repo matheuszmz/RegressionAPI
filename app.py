e4d1d39ccb2db3ee8069a6286e709febcf0ba9a2
matheuszmz <matheuszmz@hotmail.com>
2019-07-11 19:46:15 -0300

    ajuste app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ app.config['SECRET_KEY'] = '8FD1C2279CE9EE931E1BA1D2772C6'
 app.register_blueprint(page)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
